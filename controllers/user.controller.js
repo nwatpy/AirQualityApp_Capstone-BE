c83984aefc35dec7919a7dfc766231335d9d271f
Eric Schwartz <eric@developintelligence.com>
2021-11-10 20:06:14 -0500

    add ability to get single user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -102,6 +102,40 @@ const userController = {
             })
         }
 
+    },
+    //method to get all users using async/await syntax
+    getUser: async function(req, res){
+
+        //using a try/catch since we are using asyn/await and want to catch any errors if the code in the try block fails
+        try {
+
+            //get the email address of the user from the url parameters
+            const userEmail = req.params.email;
+            
+            //use our model to find the user that match a query.
+            //{email: some@email.com} is the current query which really mean find the user with that email
+            //we use await here since this is an async process and we want the code to wait for this to finish before moving on to the next line of code
+            let foundUser = await User.findOne({email: userEmail})
+
+            //if we found the user, return that user otherwise return a 404
+            if(foundUser){
+                res.json(foundUser)
+            }else{
+                res.status(404).send({
+                    status: res.statusCode,
+                    message: "User Not Found!"
+                })
+            }
+            
+        } catch (error) {
+            console.log("error getting user: " + error)
+            //if any code in the try block fails, send the user a HTTP status of 400 and a message stating we could not find the user
+            res.status(400).json({
+                message: error.message,
+                statusCode: res.statusCode
+            })
+
+        }
     }
     
 
